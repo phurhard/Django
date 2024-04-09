5873ee206859e4ea6f37ba81968039ac00f15b14
phurhard <phurhardeen@gmail.com>
2024-04-09 01:18:46 +0100

    drf-jwt is deprecated, making use of drf-simplejwt, added a requirements.txt file

--- interactquizz/interactquizz/settings.py
+++ interactquizz/interactquizz/settings.py
@@ -84,8 +84,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
 # Password validation
